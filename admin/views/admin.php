8891bfd6c171a0f7a2dc3d338b7838cc054b7d6e
Boris Aguilar <me@borisaguilar.com>
2014-05-05 13:10:32 -0600

    Small Update: Still learing to save stuff in options... i tought i allready had it solved!

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -18,8 +18,13 @@
 	<?php screen_icon(); ?>
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 	<form action="/wp-admin/network/edit.php?action=wpmu_network_ads_snippet">
-	      <label for="snippet-code">Snippet Code to use:</label>
-	      <textarea name="snippet-code" rows=4 cols=2></textarea>
+	      <label for="snippet-code"><h3>Snippet Code to use:</h3></label>
+		<p><small>This snippet is shown on every page by hooking wp_foot.</small></p>
+	      <textarea style="width:100%;" rows=10 id="snippet-code" name="snippet-code" rows=4 cols=2><?php get_option( 'wpmu_network_snippet' ); ?></textarea>
+		
+	      <label for="snippet-code-exceptions"><h3>Excluded Sites:</h3></label>
+		<p><small>One per line, don't include https or http just the URL. This URL's won't have ads.</small></p>
+	      <textarea style="width:100%;" rows=10 id="snippet-code-exceptions" name="snippet-code-exceptions" rows=4 cols=2><?php get_option( 'wpmu_network_exceptions' ); ?></textarea>
 	      <?php submit_button(); ?>
 	</form>
 
